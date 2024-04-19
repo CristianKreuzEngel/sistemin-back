87bff882f5e20c5262ea854f8721e8a1f12c297b
Cristian Kreuz Engel <kreuzengelc@gmail.com>
2024-04-18 22:32:24 -0300

    Limpando o projeto

--- resources/views/admin/cardapio/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('css')
-    <link rel="stylesheet" href="/resources/css/default.css">
-@endsection
-
-@section('content')
-<!-- tudo que pertence ao body -->
-@endsection
